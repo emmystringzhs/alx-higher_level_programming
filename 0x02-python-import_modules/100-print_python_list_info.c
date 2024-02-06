335855c2d969ab58114f26a71c447dcf08c501c6
emmystringzhs <milesbetti@gmail.com>
2024-02-06 14:10:59 +0100

    Implement C function to print Python list info as specified

--- 0x02-python-import_modules/100-print_python_list_info.c
+++ 0x02-python-import_modules/100-print_python_list_info.c
@@ -1,21 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
-/**
- * print_python_list_info - Prints information about a list
- * @p: Number of command line arguments
- */
 
+/**
+ * print_python_list_info - prints info about python lists
+ * @p: address of pyobject struct
+ */
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size, i;
-    PyListObject *list = (PyListObject *)p;
+	int i;
 
-    size = PyList_Size(p);
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", list->allocated);
-
-    for (i = 0; i < size; i++)
-    {
-        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-    }
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
-
