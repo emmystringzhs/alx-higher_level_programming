b3129fe77841c596687d9107d29f9409e4efe5d5
emmystringzhs <milesbetti@gmail.com>
2024-02-06 13:59:45 +0100

    Fix comparison operator in calculator script

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     
     operators = ["+", "-", "*", "/"]
     
-    if argv_count is not 3:
+    if argv_count != 3:
         
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         
@@ -30,18 +30,18 @@ if __name__ == "__main__":
         
         b = int(sys.argv[3])
         
-        if sys.argv[2] is "+":
+        if sys.argv[2] == "+":
             
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
             
-        elif sys.argv[2] is "-":
+        elif sys.argv[2] == "-":
             
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
             
-        elif sys.argv[2] is "*":
+        elif sys.argv[2] == "*":
             
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
             
-        elif sys.argv[2] is "/":
+        elif sys.argv[2] == "/":
             
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
